3605d7f82a0a03b56e5a9711bebbc0f01e80e8c6
Pytness <pytness@gmail.com>
2020-06-23 20:29:49 +0200

    Created game.py

---
+++ simulator/game.py
@@ -0,0 +1,41 @@
+import numpy as np
+from collections import namedtuple
+
+import utils
+from bet import Bet
+from strategies import Strategies
+
+
+from constants import *
+
+
+class NotEnoughMoney(Exception):
+	pass
+
+class GameState:
+	def __init__(self, balance: int, strategy):
+		self.__balance = balance
+		self.__strategy = strategy
+
+	def getBalance(self) -> int:
+		return self.__balance
+
+	def __placeBet(self, bet: Bet, rates, weights):
+
+		winner = np.random.choice(rates.size, p=weights)
+
+		if winner == bet.index:
+			prize = bet.amount * rates[bet.index]
+			self.__balance += prize
+		else:
+			self.__balance -= bet.amount
+
+	def nextStep(self):
+		if self.__balance <= MIN_BET:
+			raise NotEnoughMoney()
+
+		rates = utils.generateRandomRates()
+		weights = utils.getWeightsFromRates(rates)
+
+		bet = self.__strategy(self.__balance, rates, weights)
+		self.__placeBet(bet, rates, weights)
